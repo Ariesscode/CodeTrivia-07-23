b693d1ce9464a76956cdf7372b702713902f5fb1
Abigail Burtley <abigailtop95@yahoo.com>
2023-07-27 22:43:18 -0400

    add questions in javascript file

--- script.js
+++ script.js
@@ -19,6 +19,40 @@ function showName(event) {
     //add event listener to the start button and a function
     //a function to make everything on screen clear for quiz
     
-  availableQuestions = [""]
+  var quiz = [
+            { Question: "The condition in an if/else statement is enclosed with ____",
+              Choices: ["quotes", "curly brackets", "paranthesis", "square brackets"],
+              Answer: "paranthesis"
+
+            },
+
+            { Question: "String values must be enclosed within ____ when being assigned to variables.",
+              Choices: ["commas", "curly brackets", "quotes", "paranthesis"],
+              Answer: "quotes"
+
+            },
+
+            { Question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+              Choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+              Answer: "console.log"
+
+
+            },
+
+
+            { Question: "Commonly used data types Do NOT include:",
+              Choices: ["strings", "booleans", "alerts", "numbers"],
+              Answer:"alerts"
+
+
+            }
+
+        ];
+
+
+
+
+  
+  
 
     
